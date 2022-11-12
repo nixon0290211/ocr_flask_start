0fd1849ed43fe735ff66e1e052feae2d5b2cc0b7
chibatakashi <nixon0290211@icloud.com>
2022-11-12 15:15:37 +0900

    携帯のカメラで画像取得

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from PIL import Image
 from flask import Flask, render_template, request
 import pyocr
 
-# import requests
-# import json
-
 app = Flask(__name__)
 
 
@@ -28,29 +25,6 @@ def upload():
     # 読みとりエラー
     return render_template("index.html", txt=txt)
 
-    # img_en_path = "images/tesseract_en.png"
-    # img_jpn_path = "images/tesseract_jpn.png"
-
-
-#     img = Image.open(img_en_path)
-#     img = Image.open(file)
-# if request.method == "POST":
-#     if "file" not in request.files:
-#         flash("ファイルが選択されていません")
-# return redirect(request.url)
-
-# file = request.files["ocrfile"]
-
-#     if file.filename == "":
-#         flash("ファイルが選択されていません")
-#         return redirect(request.url)
-
-#     if file:
-#         outfile = paste_frame(file)
-#         return render_template("upload.html", outfile=outfile)
-
-# return redirect("/")
-
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0,0,0,0", port=5001, debug=True)
